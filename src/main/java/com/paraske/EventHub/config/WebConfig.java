56950962e9b6f1aaa38a50979484f7f3ac6b35d2
ParasMarios <paraske.marios@gmail.com>
2026-03-29 20:22:50 +0300

    Refactor CORS configuration imports

--- src/main/java/com/paraske/EventHub/config/WebConfig.java
+++ src/main/java/com/paraske/EventHub/config/WebConfig.java
@@ -4,9 +4,8 @@ package com.paraske.EventHub.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
