ca41d69e680ded2adca0ed765b0c57e47390e6c6
ParasMarios <paraske.marios@gmail.com>
2026-03-29 22:39:07 +0300

    Integrate Cloudinary for file uploads in MediaService

--- src/main/java/com/paraske/EventHub/service/MediaService.java
+++ src/main/java/com/paraske/EventHub/service/MediaService.java
@@ -1,35 +1,29 @@
 package com.paraske.EventHub.service;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.util.UUID;
+import java.util.Map;
 
 @Service
 public class MediaService {
 
-    private final String UPLOAD_DIR = "uploads/";
+    @Autowired
+    private Cloudinary cloudinary;
 
     public String saveFile(MultipartFile file) throws IOException {
-        // 1. Δημιουργία του φακέλου αν δεν υπάρχει
-        Path uploadPath = Paths.get(UPLOAD_DIR);
-        if (!Files.exists(uploadPath)) {
-            Files.createDirectories(uploadPath);
+        try {
+            Map uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.emptyMap());
+
+            return uploadResult.get("secure_url").toString();
+
+        } catch (IOException e) {
+            System.err.println("Αποτυχία ανεβάσματος κεντρικής εικόνας στο Cloudinary: " + e.getMessage());
+            throw new IOException("Αποτυχία αποθήκευσης της εικόνας στο Cloud.", e);
         }
-
-        // 2. Δημιουργία μοναδικού ονόματος αρχείου
-        String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
-        Path filePath = uploadPath.resolve(fileName);
-
-        // 3. Αντιγραφή του αρχείου στον φάκελο
-        Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
-
-        // 4. Επιστρέφουμε ΜΟΝΟ το όνομα του αρχείου για τη βάση δεδομένων
-        return fileName;
     }
 }
