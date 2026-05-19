40a0ce6fa43edc7893ec6d7aec28f74546c767e1
ParasMarios <paraske.marios@gmail.com>
2026-05-19 21:27:11 +0300

    Add JSON property annotation to password field; restrict access to write-only

--- src/main/java/com/paraske/EventHub/model/User.java
+++ src/main/java/com/paraske/EventHub/model/User.java
@@ -1,6 +1,7 @@
 package com.paraske.EventHub.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
@@ -21,6 +22,7 @@ public class User {
     private String email;
 
     @Column(nullable = false)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     @Column(columnDefinition = "TEXT")
